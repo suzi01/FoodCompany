417efa63ac963fa35c6521a0fa4781c144b5e5e3
suzannah <suzannahthompson@hotmail.co.uk>
2025-08-23 20:06:23 +0100

    add testing for mantine

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -10,7 +10,8 @@ const config: StorybookConfig = {
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
+    'storybook-dark-mode',
   ],
   "framework": {
     "name": "@storybook/react-vite",
