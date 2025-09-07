ade0c477886dac6c53df404843033b43a5fa6481
suzannah <suzannahthompson@hotmail.co.uk>
2025-09-07 12:02:31 +0100

    [supplier endpoints]  create a model for supplier and add comments for controller

---
+++ backend/src/controllers/supplierController.ts
@@ -0,0 +1,7 @@
+import Supplier from "../models/supplier";
+// - **Read**: Display a list of all suppliers.
+// - **Search**: Filter suppliers by name, items they provide (through a product search), or unique code (which you could add, such as a supplier ID).
+// - **Create**: Add a new supplier.
+// - **Update**: Modify an existing supplier's details.
+// - **Delete**: Remove a supplier.
+
---
+++ backend/src/models/supplier.ts
@@ -0,0 +1,61 @@
+import { Schema, model, Document, Types } from 'mongoose';
+
+export interface ISupplier extends Document {
+    companyName: string;
+    mainContactName: string;
+    address?: string;
+    email: string,
+    phoneNumber?:string,
+    productsProvided: Types.ObjectId[];
+    branches:Types.ObjectId[]
+}
+
+const supplierSchema = new Schema<ISupplier>({
+  companyName: {
+    type: String,
+    required: [true, 'Company name is required'],
+    trim: true,
+    minLength: 2,
+    maxLength: 50,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Company email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+  },
+  mainContactName: {
+    type: String,
+    trim: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  address: {
+    type: String,
+    trim: true,
+    maxLength: 70,
+  },
+  phoneNumber: {
+    type: String,
+    match: /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/,
+  },
+  productsProvided: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: false,
+   
+  }],
+  branches: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Branch',
+    required: false,
+   
+  }],
+}, { timestamps: true });
+
+const Supplier = model('Supplier', supplierSchema);
+
+export default Supplier;
