6fc09a25d1ee0fcfccbad9c6aef3cef411c37264
suzannah <suzannahthompson@hotmail.co.uk>
2025-11-20 17:13:35 +0000

    Refactor Header component layout: enhance structure and styling for improved responsiveness

--- frontend/src/components/Layout/Header/Header.tsx
+++ frontend/src/components/Layout/Header/Header.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center p-4 border-b border-gray-300 bg-white">
-      <div>Burger menu</div>
-      <div className="text-2xl">NaturalFoods</div>
-      <div className="text-xs font-normal">
-        <div>Janine Wilson</div>
-        <div>Janine.wilson@food.com</div>
+    <div className="flex border-b border-gray-300 bg-white justify-center">
+      <div className="flex justify-between items-center p-4 flex-row w-full max-w-7xl">
+        <div>
+          <p>Burger menu</p>
+        </div>
+        <div className="text-2xl">
+          <p>NaturalFoods</p>
+        </div>
+        <div className="text-xs font-normal">
+          <div>
+            <p>Janine Wilson</p>
+          </div>
+          <div>
+            <p>Janine.wilson@food.com</p>
+          </div>
+        </div>
       </div>
     </div>
   );
