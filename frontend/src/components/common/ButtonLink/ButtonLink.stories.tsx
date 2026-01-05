dfa6cdf8eab064e1fec6304da3a58dcd83c77979
suzannah <suzannahthompson@hotmail.co.uk>
2026-01-05 17:04:49 +0000

    Add ButtonLink component with stories and tests: implement rendering and styling logic

---
+++ frontend/src/components/common/ButtonLink/ButtonLink.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { MemoryRouter } from 'react-router-dom';
+
+import { ButtonLink } from './ButtonLink';
+
+type Story = StoryObj<typeof ButtonLink>;
+
+const meta: Meta<typeof ButtonLink> = {
+  title: 'ButtonLink',
+  component: ButtonLink,
+  tags: ['autodocs'],
+  render: ({ ...args }) => (
+    <MemoryRouter>
+      <ButtonLink {...args} />
+    </MemoryRouter>
+  ),
+};
+export default meta;
+
+export const BasicButtonLink: Story = {
+  args: {
+    to: '/example',
+    children: 'This is a button link',
+  },
+};
+
+export const StyledButtonLink: Story = {
+  args: {
+    to: '/example',
+    className: 'text-orange-200',
+    children: 'This is a styled button link',
+  },
+};
---
+++ frontend/src/components/common/ButtonLink/ButtonLink.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+
+import { render } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+
+import { ButtonLink } from './ButtonLink';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('ButtonLink.tsx', () => {
+  it('renders ButtonLink', () => {
+    render(
+      <MemoryRouter>
+        <ButtonLink to="/suppliers">
+          <p>Test text</p>
+        </ButtonLink>
+      </MemoryRouter>,
+    );
+    const testText = screen.getByText('Test text');
+    expect(testText).toBeInTheDocument();
+  });
+
+  it('renders ButtonLink with pink border', () => {
+    render(
+      <MemoryRouter>
+        <ButtonLink to="/suppliers" className="text-pink-500">
+          <p>Test text</p>
+        </ButtonLink>
+      </MemoryRouter>,
+    );
+
+    const testText = screen.getByText('Test text');
+    expect(testText.parentElement).toHaveClass('text-pink-500');
+  });
+});
