999235e4eb1631fe6b301eb81118080ee14d3700
suzannah <suzannahthompson@hotmail.co.uk>
2026-01-07 17:12:58 +0000

    Add data factory functions for Branch, Product, and Supplier models

---
+++ frontend/src/testUtils/dataFactories/Branch/Branch.ts
@@ -0,0 +1,41 @@
+import { faker } from '@faker-js/faker';
+import { Branch, GetBranchesResponse } from '@/models/Branch';
+
+export const createBranch = (overrides = {}) => {
+  return {
+    id: faker.string.uuid(),
+    branchName: faker.company.name(),
+    mainContactName: faker.person.fullName(),
+    mainContactPhoneNumber: faker.phone.number(),
+    mainContactEmail: faker.internet.email(),
+    branchEmail: faker.internet.email(),
+    phoneNumber: faker.phone.number(),
+    address: faker.location.streetAddress(),
+    yearsActive: faker.number.int({ min: 0, max: 100 }),
+    suppliers: [],
+    createdAt: faker.date.past().toISOString(),
+    updatedAt: faker.date.recent().toISOString(),
+    ...overrides,
+  };
+};
+
+export const createBranches = (count: number): Branch[] => {
+  const newBranches: Branch[] = [];
+  for (let i = 0; i < count; i++) {
+    newBranches.push(createBranch());
+  }
+  return newBranches;
+};
+
+export const createGetBranchesResponse = (
+  count: number,
+): GetBranchesResponse => {
+  const newBranches: Branch[] = [];
+  for (let i = 0; i < count; i++) {
+    newBranches.push(createBranch());
+  }
+  return {
+    success: true,
+    data: newBranches,
+  };
+};
