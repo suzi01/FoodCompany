11fd63711dd3e4bf114bf9d81a761f88f6efb821
suzannah <suzannahthompson@hotmail.co.uk>
2026-01-16 16:38:28 +0000

    Refactor Layout tests to simplify rendering and remove unnecessary routes

--- frontend/src/components/Layout/Layout/Layout.test.tsx
+++ frontend/src/components/Layout/Layout/Layout.test.tsx
@@ -1,20 +1,15 @@
 import '@testing-library/jest-dom';
 
-import { screen } from '@testing-library/dom';
-import { render } from '@testing-library/react';
+import { screen, render } from '@/testUtils';
 
-import { Breadcrumb } from '@/components/common/Breadcrumb/Breadcrumb';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { Layout } from './Layout';
 
 describe('Layout.tsx', () => {
   it('renders Layout correctly', () => {
     render(
       <MemoryRouter initialEntries={['/']}>
-        <Routes>
-          <Route path="*" element={<Breadcrumb />} />
-          <Layout />
-        </Routes>
+        <Layout />
       </MemoryRouter>,
     );
     const mainTitle = screen.getAllByText(/naturalfoods/i);
--- frontend/src/contants/routes.ts
+++ frontend/src/contants/routes.ts
@@ -3,4 +3,11 @@ export const ROUTES = {
   BRANCHES: '/branches',
   SUPPLIERS: '/suppliers',
   PRODUCTS: '/products',
+  BRANCHE_DETAILS: '/branches/:id',
+  SUPPLIER_DETAILS: '/suppliers/:id',
+  PRODUCT_DETAILS: '/products/:id',
+  BRANCHES_EDIT: '/branches/:id/edit',
+  SUPPLIERS_EDIT: '/suppliers/:id/edit',
+  PRODUCTS_EDIT: '/products/:id/edit',
+  ADD_SUPPLIER_TO_BRANCH: '/branches/:id/add-supplier',
 };
