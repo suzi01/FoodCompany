51a38b4ae086203a4bfcd6175cbacd20a4624f9d
suzannah <suzannahthompson@hotmail.co.uk>
2026-01-20 17:26:15 +0000

    Add useUrlFilters hook for managing URL search parameters and filters

---
+++ frontend/src/hooks/useUrlFilters/useUrlFilters.ts
@@ -0,0 +1,17 @@
+import { useSearchParams } from 'react-router-dom';
+
+export const useUrlFilters = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const updateFilter = (key: string, value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set(key, value);
+    } else {
+      params.delete(key);
+    }
+    setSearchParams(params);
+  };
+
+  return { searchParams, updateFilter };
+};
