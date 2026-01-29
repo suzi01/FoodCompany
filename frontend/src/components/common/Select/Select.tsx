e1d43a1ccf2a2f4fe7aecc62cddb4e0c08f3e0eb
suzannah <suzannahthompson@hotmail.co.uk>
2026-01-29 15:35:08 +0000

    Add Select component with stories and tests

--- frontend/src/components/common/Select/Select.tsx
+++ frontend/src/components/common/Select/Select.tsx
@@ -1,20 +1,23 @@
-interface SelectProps {
+import { SelectHTMLAttributes } from "react";
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
   options: string[];
   label: string;
   name: string;
   className?: string;
 }
 
-export const Select = ({ options, label, name, className }: SelectProps) => {
+export const Select = ({ options, label, name, className, ...props }: SelectProps) => {
   return (
     <div className={`flex flex-col w-full gap-1 ${className}`}>
-      <label htmlFor={name} className="text-gray-500">
+      <label htmlFor={name} className="text-gray-500 uppercase font-medium">
         {label}
       </label>
       <select
         id={name}
         name={name}
         className="w-full border border-gray-300 rounded-md p-2 mb-4"
+        {...props}
       >
         {options.map((option, index) => (
           <option key={`${index}-${option}`} value={option}>
