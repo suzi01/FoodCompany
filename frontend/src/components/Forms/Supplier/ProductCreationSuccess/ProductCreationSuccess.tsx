6b379bbf4124e7c9632003afb99feaf6f9e70e4e
suzannah <suzannahthompson@hotmail.co.uk>
2026-03-04 15:27:14 +0000

    Add ProductCreationSuccess component; implement success message for supplier linking

---
+++ frontend/src/components/Forms/Supplier/ProductCreationSuccess/ProductCreationSuccess.tsx
@@ -0,0 +1,12 @@
+export const ProductCreationSuccess = () => {
+  return (
+    <div>
+      <p>tick</p>
+      <p>Link confirmed!</p>
+      <p>
+        The supplier has been successfully linked to the product. All associated
+        products have been added and are active.
+      </p>
+    </div>
+  );
+};
